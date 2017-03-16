015fba7b1f3e3edc3e4fbebf9232f86965459e64
ziyi <ziyi93@yeah.net>
2017-03-16 18:40:58 +0800

    cut image

--- read.py
+++ read.py
@@ -32,7 +32,7 @@ def open(fp):
     return Image.open(fp)
 
 
-class Pure:
+class Read:
     '''
     Purify picture, remove noise dot.
     '''
@@ -44,15 +44,8 @@ class Pure:
         '''
 
         self.im = im.convert('L')
-        self.load()
-
-    def load(self):
-        '''
-        update current image size
-
-        :return:None
-        '''
         self.size = self.im.size
+        self.ch_list = [] # character image
 
     def crop(self, box=None):
         '''
@@ -74,7 +67,7 @@ class Pure:
         :return:None
         '''
 
-        self.load()
+        self.size = self.im.size
         self.im = self.im.point(lambda i: 0 if (i < value) else 255)
 
     def denoising(self):
@@ -83,38 +76,120 @@ class Pure:
         :return:
         '''
 
-        self.load()
-        px = self.im.load()
-        print(self.size)
-        for i in range(self.size[0]):
-            self.im.put
+        self.size = self.im.size
 
-        # for i in range(self.size[0]):
-        #     for j in range(self.size[1]):
-        #         print(px[i, j])
+        # remove the noise in the frame of image
+        for i in range(self.size[0]):
+            self.im.putpixel((i, 0), 255)
+            self.im.putpixel((i, self.size[1]-1), 255)
+        for j in range(self.size[1]):
+            self.im.putpixel((0, j), 255)
+            self.im.putpixel((self.size[0]-1, j), 255)
+
+        # remove single noise
+        px = self.im.load()
+        for i in range(1, self.size[0]-1):
+            for j in range(1, self.size[1]-1):
+                if px[i,j] == 0:
+                    if px[i, j-1] == 255 and \
+                       px[i, j+1] == 255 and \
+                       px[i-1, j] == 255 and \
+                       px[i+1, j] ==255:
+                        self.im.putpixel((i, j), 255)
+                    elif px[i + 1, j] == 0 and \
+                          px[i + 2, j] == 255 and \
+                          px[i + 1, j + 1] == 255 and \
+                          px[i + 1, j - 1] == 255 and \
+                          px[i, j + 1] == 255 and \
+                          px[i, j - 1] == 255 and \
+                          px[i - 1, j] == 255:
+                        self.im.putpixel((i, j), 255)
+                        self.im.putpixel((i + 1, j), 255)
+                    elif px[i, j + 1] == 0 and \
+                          px[i, j + 2] == 255 and \
+                          px[i + 1, j + 1] == 255 and \
+                          px[i - 1, j + 1] == 255 and \
+                          px[i + 1, j] == 255 and \
+                          px[i - 1, j] == 255 and \
+                          px[i, j - 1] == 255:
+                        self.im.putpixel((i, j), 255)
+                        self.im.putpixel((i, j + 1), 255)
+
+    def cut(self):
+        '''
+        sperate each character
+        :return:
+        '''
+
+        px = self.im.load()
+
+        # get character 'x' coordinate
+        x_list = []
+        for i in range(1, self.size[0]-1):
+            for j in range(1, self.size[1]-1):
+                if px[i,j] == 0:
+                    x_list.append(i)
+                    break
+
+        if len(x_list) < 10:
+            raise Exception('no character detected')
+
+        c = []
+        c.append(x_list[0])
+        for k in range(1,len(x_list)-1):
+            if x_list[k - 1] + 1 != x_list[k]:
+                c.append(x_list[k - 1])
+                c.append(x_list[k])
+        c.append(x_list[-1])
+
+        if len(c) % 2 != 0:
+            raise Exception('bad image')
+
+        w = self.size[1]
+        ch_list = []
+        for h in range(0, len(c), 2):
+            x = c[h]
+            y = c[h + 1]+1
+            ch_list.append(self.im.crop((x, 0, y, w)))
+
+        for ch in ch_list:
+            ch_px = ch.load()
+            ch_size = ch.size
+            l = ch_size[0]
+            y_list = []
+            for j in range(ch_size[1]):
+                for i in range(ch_size[0]):
+                    if ch_px[i, j] == 0:
+                        y_list.append(j)
+                        break
+            x = y_list[0]
+            y = y_list[-1]+1
+            self.ch_list.append(ch.crop((0, x, l, y)))
+
+        n = len(self.ch_list)
+        if n < 4:
+            self.separate(n)
+        elif n > 4:
+            raise Exception('more than 4 characters, must be wrong')
+
+    def separate(self, n):
+        for i in range(n):
+            size = self.ch_list[i].size
+            int(size[0] / 10)
+        pass
 
     def show(self):
         self.im.show()
 
-    def test(self):
-        # Image.blend
-        pass
-
-
-class Cut:
-    pass
-
-
-class Read:
-    pass
-
 
 if __name__ == '__main__':
-    im = open('sample/qiangzhi/MZWW7PC4JI')
-    im_obj = Pure(im)
+    im = open('sample/qiangzhi/ZRQBPXOGRQ')
+    im_obj = Read(im)
     # im_obj.show() # original picture in 'L' mode
     im_obj.crop((5, 5, 65, 22))
     # im_obj.show() # crop characters region
     im_obj.binaryzation(100)
     # im_obj.show() # set to 2 value
     im_obj.denoising()
+    # im_obj.show() # after denoiseing
+    im_obj.cut()
